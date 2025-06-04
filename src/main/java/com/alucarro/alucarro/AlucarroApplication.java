eeac5fd473f173c92fc7d53289d1e7079446486e
FernandoBersellini <fernandobersellini@outlook.com.br>
2025-06-04 20:20:40 -0300

    :tada: Primeiro commit!

---
+++ src/main/java/com/alucarro/alucarro/AlucarroApplication.java
@@ -0,0 +1,13 @@
+package com.alucarro.alucarro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlucarroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlucarroApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/alucarro/alucarro/AlucarroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alucarro.alucarro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlucarroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
